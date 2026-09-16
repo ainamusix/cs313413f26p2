aea5b8ef88332c677bb2adef8dd35949f9a1dfc1
kristianabusog <kristianabusog@gmail.com>
2026-09-15 21:34:28 -0500

    Change list type

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -18,8 +18,8 @@ public class TestIterator {
 
   @BeforeEach
   public void setUp() throws Exception {
-    list = new LinkedList<Integer>();
-    // list = new ArrayList<Integer>();
+    //list = new LinkedList<Integer>();
+    list = new ArrayList<Integer>();
     // TODO Question: Also try with a LinkedList - does it make any difference?
     //
   }
